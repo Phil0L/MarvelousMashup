c28cd758fd8dea7ba592d42e923c1877a87f7d4a
Corneilus Huber <cornelius.huber@uni-ulm.de>
2021-06-03 23:07:32 +0200

    Feat: Added mockup for save/load dialog

--- MarvelousMashupEditorTeam16/Assets/Scripts/PartyStore.cs
+++ MarvelousMashupEditorTeam16/Assets/Scripts/PartyStore.cs
@@ -1,6 +1,17 @@
+using System;
 using UnityEngine;
-public class PartyStore:MonoBehaviour  //, Store
+
+
+public class PartyStore:MonoBehaviour, IStore
 {
     public Party party;
+    
 
+    public void loadJson(string json){
+        throw new NotImplementedException();
+	}
+
+    public string toJson() {
+        throw new NotImplementedException();
+	}
 }
