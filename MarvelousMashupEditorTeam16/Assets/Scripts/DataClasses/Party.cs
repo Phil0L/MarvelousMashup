dc3f6e5d916de5765449df614486caf502e2e7b3
Corneilus Huber <cornelius.huber@uni-ulm.de>
2021-06-03 22:00:20 +0200

    feat: Added names for the party-config value changers

--- MarvelousMashupEditorTeam16/Assets/Scripts/DataClasses/Party.cs
+++ MarvelousMashupEditorTeam16/Assets/Scripts/DataClasses/Party.cs
@@ -1,5 +1,18 @@
 public class Party
 {
     public int spaceStoneCD;
-    // hier alle values
+	public int realityStoneCD;
+	public int mindStoneCD;
+	public int powerStoneCD;
+	public int timeStoneCD;
+	public int soulStoneCD;
+	public int mindStoneDMG;
+	
+	public int maxGameTime;
+	public int maxAnimationTime;
+	public int maxResponseTime;
+	public int maxPauseTime;
+	public int maxRoundTime;
+	public int maxRounds;
+
 }
