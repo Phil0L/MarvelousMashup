2e47384afee7286b15238f374b57371e20aaf25d
phill <philipp.letschka@uni-ulm.de>
2021-06-17 16:32:53 +0100

    Feat: Server testing

--- MarvelousMashupServerTeam23/src/main/java/communication/NetworkHandler.java
+++ MarvelousMashupServerTeam23/src/main/java/communication/NetworkHandler.java
@@ -108,14 +108,16 @@ public class NetworkHandler extends WebSocketServer {
 
         controller.SpectatorList.remove(conn);
         System.out.println("SERVER: Connection to " + ((conn.getAttachment()) == null ? "unknown remote" :
+            ((Attachment) (conn.getAttachment())).profile == null ? "unknown profile" :
                 ((Attachment) (conn.getAttachment())).profile.name + ":" +
                         ((Attachment) (conn.getAttachment())).profile.deviceID) + " closed");
 
         //if the disconnected WebSocket was associated with a Player, start the timer for the reconnect
-        if(!(conn.getAttachment() == null)){
-            controller.turnTimer.stop();
+        if(conn.getAttachment() != null){
+            if (controller != null && controller.turnTimer != null)
+                controller.turnTimer.stop();
             Attachment attachment = conn.getAttachment();
-            if(attachment != null && (checkPlayerIdentity(attachment.profile)) != null){
+            if(attachment != null && attachment.profile != null && (checkPlayerIdentity(attachment.profile)) != null){
                 Player player = checkPlayerIdentity(attachment.profile);
                 player.timeoutTimer = new SimpleTimer(configuration.matchConfig.maxResponseTime) {
                     @Override
@@ -567,13 +569,15 @@ public class NetworkHandler extends WebSocketServer {
      * @return method returns the player the profile is associated with or null if there is no player with the same profile found
      */
     public Player checkPlayerIdentity(Profile profile){
-        if(controller.model.playerOne.profile.equals(profile)){   //check first player profile
-            return controller.model.playerOne;
-        }
-        else if(controller.model.playerTwo.profile.equals(profile)){   //check second player profile
-            return controller.model.playerTwo;
-        }
-        else{      //profile doesn't belong to a player --> return null
+        try {
+            if (controller.model.playerOne.profile.equals(profile)) {   //check first player profile
+                return controller.model.playerOne;
+            } else if (controller.model.playerTwo.profile.equals(profile)) {   //check second player profile
+                return controller.model.playerTwo;
+            } else {      //profile doesn't belong to a player --> return null
+                return null;
+            }
+        }catch (NullPointerException e){
             return null;
         }
     }
