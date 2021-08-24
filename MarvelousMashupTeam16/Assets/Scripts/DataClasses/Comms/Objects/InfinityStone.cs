ca035301d6a816b4af18381b0331007c53dd3441
Corneilus Huber <cornelius.huber@uni-ulm.de>
2021-08-24 18:07:08 +0200

    Refactor: Unified InfinityStones class

--- MarvelousMashupTeam16/Assets/Scripts/DataClasses/Comms/Objects/InfinityStone.cs
+++ MarvelousMashupTeam16/Assets/Scripts/DataClasses/Comms/Objects/InfinityStone.cs
@@ -1,6 +1,17 @@
-public class InfinityStone
+public class InfinityStone : Entities, IFieldContent
 {
     
+    public const int GREEN = 1;
+    public const int YELLOW = 2;
+    public const int PURPLE = 3;
+    public const int BLUE = 4;
+    public const int RED = 5;
+    public const int ORANGE = 6;
+
+
+    public int stone;
+    public int cooldown;
+    public int defaultcooldownTime;
 
     /**
      * the constructor of the InfinityStone-Class. Creates a InfinityStone-MessageObject.
@@ -28,6 +39,17 @@ public class InfinityStone
      */
     public override bool Equals(object o) {
         return super.equals(o);
+    }
+    
+    public override string ToString()
+    {
+        return (stone == GREEN ? "Green" :
+                   stone == YELLOW ? "Yellow" :
+                   stone == PURPLE ? "Purple" :
+                   stone == BLUE ? "Blue" :
+                   stone == RED ? "Red" :
+                   stone == ORANGE ? "Orange" : "Undefined")
+               + " InfinityStone";
     }
     
 }
